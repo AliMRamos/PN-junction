c15dd81c845dd18ebfb2d38c5ff08edd33f6a64b
AliMRamos <alidsjt@gmail.com>
2020-04-12 02:58:08 +0200

    Upgrade the writing of the output file

--- Calculations.py
+++ Calculations.py
@@ -1,41 +1,35 @@
-'''
-Alicia M. Ramos, contacto munozramosalicia@gmail.com
-Calculos teoricos para una union PN de Si
-Valores tomados de http://www.ioffe.ru/SVA/NSM/Semicond/Si/index.html
-'''
 import math as m
 import numpy as np
 import scipy.constants as sc
 
 cte_boltz_J = sc.physical_constants["Boltzmann constant"][0]  # J/K
 cte_boltz_eV = sc.physical_constants["Boltzmann constant in eV/K"][0]  # eV/K
+permittivity = sc.epsilon_0 / 100.  # F/cm
 q = sc.e  # C
-m_h = 0.81  # *m_0
-m_e = 1.18  # *m_0
 
 
-def concentracion_intrinseca(temperatura, energia_gap):
-    N_C = 6. * (0.36 ** (3. / 2.)) * (temperatura ** (3. / 2.)) * 4.8286 * (10. ** 15.)  # cm-3
-    N_V = (0.81 ** (3. / 2.)) * (temperatura ** (3. / 2.)) * 4.8286 * (10. ** 15.)  # cm-3
-    n_i = m.sqrt(N_C * N_V) * m.e ** (-energia_gap / (2. * cte_boltz_eV * temperatura))  # cm-3
+def intrinsic_concentration(temperature, gap_energy):
+    N_C = 6. * (0.36 ** (3. / 2.)) * (temperature ** (3. / 2.)) * 4.8286 * (10. ** 15.)  # cm-3
+    N_V = (0.81 ** (3. / 2.)) * (temperature ** (3. / 2.)) * 4.8286 * (10. ** 15.)  # cm-3
+    n_i = m.sqrt(N_C * N_V) * m.e ** (-gap_energy / (2. * cte_boltz_eV * temperature))  # cm-3
     return n_i, N_V, N_C
 
 
-def portadores(concentr_intrins, donadores, aceptores):
-    p_p = aceptores
-    n_n = donadores
-    p_n = (concentr_intrins ** 2.) / donadores
-    n_p = (concentr_intrins ** 2.) / aceptores
+def carriers(intrins_concentr, donors, acceptors):
+    p_p = acceptors
+    n_n = donors
+    p_n = (intrins_concentr ** 2.) / donors
+    n_p = (intrins_concentr ** 2.) / acceptors
     return p_p, n_n, p_n, n_p
 
 
-def potenical_contacto(donadores, aceptores, concentr_intrins, temperatura):
-    V_bi = (cte_boltz_J * temperatura / q) * m.log(donadores * aceptores / (concentr_intrins ** 2.))  # V
+def built_in_potencial(donors, acceptors, intrins_concentr, temperature):
+    V_bi = (cte_boltz_J * temperature / q) * m.log(donors * acceptors / (intrins_concentr ** 2.))  # V
     return V_bi
 
 
-def energia_maxima(donadores, x_n, cte_dielectrica):
-    E_max = -q * donadores * x_n / cte_dielectrica  # V/cm
+def field_maximum(donors, x_n, relative_permittivity):
+    E_max = -q * donors * x_n / (permittivity * relative_permittivity)  # V/cm
     return E_max
 
 
@@ -52,19 +46,21 @@ Out of equilibrium:
 '''
 
 
-def bandas(energia_gap, temperatura, aceptores, concentr_instrins, donadores, N_V):
+def bands(gap_energy, temperature, acceptors, concentr_instrins, donors, N_V, effective_mass_holes,
+          effective_mass_electrons):
     E_V = 0.
-    E_C = energia_gap + E_V
-    E_i = E_V + (energia_gap / 2.) + (3. * cte_boltz_eV * temperatura / 4.) * m.log(m_h / m_e)
-    E_F = E_V - (cte_boltz_eV * temperatura * m.log(aceptores / N_V))
-    if E_C - E_F > 3. * cte_boltz_eV * temperatura and E_F - E_V > 3. * cte_boltz_eV * temperatura:
-        print('Es no degenerado')
+    E_C = gap_energy + E_V
+    E_i = E_V + (gap_energy / 2.) + (3. * cte_boltz_eV * temperature / 4.) * m.log(
+        effective_mass_holes / effective_mass_electrons)
+    E_F = E_V - (cte_boltz_eV * temperature * m.log(acceptors / N_V))
+    if E_C - E_F > 3. * cte_boltz_eV * temperature and E_F - E_V > 3. * cte_boltz_eV * temperature:
+        print('Nondegenerate Semiconductor')
     else:
-        print('Es degenerado, no se puede aproximar')
+        print('Degenerate Semiconductor, the approximations taken into a count can not be used')
         exit(1)
     # Fermi level
-    p_type = cte_boltz_eV * temperatura * m.log(concentr_instrins / aceptores)
-    n_type = cte_boltz_eV * temperatura * m.log(donadores / concentr_instrins)
+    p_type = cte_boltz_eV * temperature * m.log(concentr_instrins / acceptors)
+    n_type = cte_boltz_eV * temperature * m.log(donors / concentr_instrins)
     return E_i - E_V, E_C - E_i, p_type, n_type
 
 
@@ -73,17 +69,19 @@ Case for V_bi=0, none forward nor reverse bias has been taken into account
 '''
 
 
-def vaciamiento_n(cte_dielectrica, V_bi, donadores, aceptores):
-    x_n = m.sqrt(2. * cte_dielectrica * V_bi * aceptores / (q * donadores * (aceptores + donadores)))  # cm
+def depletion_n(relative_permittivity, V_bi, donors, acceptors):
+    x_n = m.sqrt(
+        2. * permittivity * relative_permittivity * V_bi * acceptors / (q * donors * (acceptors + donors)))  # cm
     return x_n
 
 
-def vaciamiento_p(cte_dielectrica, V_bi, donadores, aceptores):
-    x_p = m.sqrt(2. * cte_dielectrica * V_bi * donadores / (q * aceptores * (aceptores + donadores)))  # cm
+def depletion_p(relative_permittivity, V_bi, donors, acceptors):
+    x_p = m.sqrt(
+        2. * (permittivity * relative_permittivity) * V_bi * donors / (q * acceptors * (acceptors + donors)))  # cm
     return x_p
 
 
-def ancho_vaciamiento(x_n, x_p):
+def depletion_zone(x_n, x_p):
     W = x_p + x_n  # cm
     return W
 
@@ -95,51 +93,55 @@ Applying forward and reverse bias:
 '''
 
 
-def bias(V_bi, polarizacion):
-    V_j = V_bi - polarizacion
+def bias(V_bi, bias):
+    V_j = V_bi - bias
     return V_j
 
 
-def vaciamiento_n_bias(cte_dielectrica, V_j, donadores, aceptores):
-    x_n_bias = m.sqrt(2. * cte_dielectrica * V_j * aceptores / (q * donadores * (aceptores + donadores)))
+def depletion_n_bias(relative_permittivity, V_j, donors, acceptors):
+    x_n_bias = m.sqrt(
+        2. * (permittivity * relative_permittivity) * V_j * acceptors / (q * donors * (acceptors + donors)))
     return x_n_bias
 
 
-def vaciamiento_p_bias(cte_dielectrica, V_j, donadores, aceptores):
-    x_p_bias = m.sqrt(2. * cte_dielectrica * V_j * donadores / (q * aceptores * (aceptores + donadores)))
+def depletion_p_bias(relative_permittivity, V_j, donors, acceptors):
+    x_p_bias = m.sqrt(2. * permittivity * relative_permittivity * V_j * donors / (q * acceptors * (acceptors + donors)))
     return x_p_bias
 
 
-def ancho_vaciamiento_bias(cte_dielectrica, V_j, donadores):
-    W_bias = m.sqrt(2. * cte_dielectrica * V_j / (q * donadores))
+def depletion_zone_bias(relative_permittivity, V_j, donors):
+    W_bias = m.sqrt(2. * (permittivity * relative_permittivity) * V_j / (q * donors))
     return W_bias
 
 
-def energia_maxima_bias(donadores, x_n_bias, cte_dielectrica):
-    E_max_bias = -q * donadores * x_n_bias / cte_dielectrica  # V/cm
+def field_maximum_bias(donors, x_n_bias, relative_permittivity):
+    E_max_bias = -q * donors * x_n_bias / (permittivity * relative_permittivity)  # V/cm
     return E_max_bias
 
 
-def desplazamiento_bandas(n_type, p_type, V_bi, polarizacion):
+def displacement_bands(n_type, p_type, V_bi, bias):
     # Quasi Fermi levels
     quasi_f = n_type - p_type
     quasi_f_2 = V_bi
-    fermi = polarizacion
+    fermi = bias
     return quasi_f, quasi_f_2, fermi
 
 
-def densidad_corriente(polarizacion1, polarizacion2, temperatura, n_p, p_n):
+def current_flux(bias1, bias2, temperature, n_p, p_n):
     corriente = []
-    D_n = cte_boltz_eV * temperatura * 1488
+    D_n = cte_boltz_eV * temperature * 1488
     L_n = m.sqrt(D_n * 1 * (10 ** -10))
-    D_p = cte_boltz_eV * temperatura * 2169
+    D_p = cte_boltz_eV * temperature * 2169
     L_p = m.sqrt(D_p * 1 * (10 ** -10))
-    voltage = np.linspace(polarizacion1, polarizacion2, num=20)
+    voltage = np.linspace(bias1, bias2, num=20)
     voltage = list(voltage)
-    for indx, polarizacion in enumerate(voltage):
+    for indx, bias in enumerate(voltage):
         J = q * (((D_n / L_n) * n_p) + ((D_p / L_p) * p_n)) * (
-                    (m.e ** (polarizacion / (cte_boltz_eV * temperatura))) - 1)
+                (m.e ** (bias / (cte_boltz_eV * temperature))) - 1)
         corriente.append([voltage[indx], J])
-    return corriente
-
+    fil = open("corrientes.txt", "w")
+    encabezado = ["------------------------------------------------------------ \n Current \n ------------------------------------------------------------ \nVoltage (V), Current (A/cm2) \n"]
+    fil.writelines(encabezado)
+    fil.writelines([str(i)+"\n" for i in corriente])
+    fil.close()
 
--- main.py
+++ main.py
@@ -1,42 +1,92 @@
 import os
+import re
 import argparse
 import Calculations as cal
 
 parser = argparse.ArgumentParser(description="Obtain the theoretical results for a PN junction of Si")
-parser.add_argument('-n', metavar='file_name', help='specify this option to rename the file.', default=None)
+parser.add_argument('-i', metavar='config_file', help='path to the config.txt', default=None)
+parser.add_argument('-o', metavar='file_name', help='specify this option to rename the file.')
+parser.add_argument('-t', metavar='temperature', help='temperature for the simulation in [K].', type=float, default=300.)
+parser.add_argument('-mh', metavar='effective_mass_holes', help='effective mass of the holes for the material.',
+                    type=float, default=0.81)
+parser.add_argument('-me', metavar='effective_mass_electrons', help='effective mass of the electrons for the material.',
+                    type=float, default=1.18)
+parser.add_argument('-ge', metavar='energy_gap', help='energy gap of the material [eV].', type=float, default=1.12)
+parser.add_argument('-rp', metavar='relative_permittivity', help='relative permittivity of the material.', type=float,
+                    default=11.8)
+parser.add_argument('-d', metavar='donors', help='specify the donors [cm-3] with the format 4.2e15', type=float,
+                    default=5.0e16)
+parser.add_argument('-a', metavar='acceptors', help='specify the acceptors [cm-3] with the format 6.7e16', type=float,
+                    default=5.0e16)
+parser.add_argument('-b', metavar='bias', help='specify the bias [V]: forward 0.2, reverse -0.3', type=float,
+                    default=0.0)
 args = parser.parse_args()
 
-'---------------------------DATOS---------------------------'
-temperatura = 300.  # K
-energia_gap = 1.12  # eV
-cte_dielectrica = 1.044772 * (10. ** -12.)  # F/cm
 
-donadores = 5.4 * (10. ** 16)  # cm-3
-aceptores = 5.4 * (10. ** 17)  # cm-3
-polarizacion = 0.3  # V
+def get_config(config_file):
+    parameters = []
+    with open(config_file, 'r') as cf:
+        regex_parameters = re.compile(r'^(\S*)\=(.*)$')
+        for line in cf:
+            line = line.strip().replace(" ", "")
+            if not line.startswith("#") and not line.startswith("\n"):
+                match = regex_parameters.match(line)
+                parameters.append((match.group(1), match.group(2)))
+    result = {}
+    for l in parameters:
+        result[l[0]] = l[1]
+    return result
 
-'-----------------------------------------------------------'
 
-n_i, N_V, N_C = cal.concentracion_intrinseca(temperatura, energia_gap)
-p_p, n_n, p_n, n_p = cal.portadores(n_i, donadores, aceptores)
-V_bi = cal.potenical_contacto(donadores, aceptores, n_i, temperatura)
-x_n = cal.vaciamiento_n(cte_dielectrica, V_bi, donadores, aceptores)
-x_p = cal.vaciamiento_p(cte_dielectrica, V_bi, donadores, aceptores)
-W = cal.ancho_vaciamiento(x_n, x_p)
-E_max = cal.energia_maxima(donadores, x_n, cte_dielectrica)
-ValToIn, CondToIn, p_type, n_type = cal.bandas(energia_gap, temperatura, aceptores, n_i, donadores, N_V)
-quasi_f, quasi_f_2, fermi = cal.desplazamiento_bandas(n_type, p_type, V_bi, polarizacion)
+if args.i is None:
+    effective_mass_holes     = args.mh
+    effective_mass_electrons = args.me
+    temperature              = args.t
+    gap_energy               = args.ge
+    relative_permittivity    = args.rp
+    donors                   = args.d
+    acceptors                = args.a
+    bias                     = args.b
 
-# Aplicamos polarizacion
-V_j = cal.bias(V_bi, polarizacion)
-x_p_bias = cal.vaciamiento_p_bias(cte_dielectrica, V_j, donadores, aceptores)
-x_n_bias = cal.vaciamiento_n_bias(cte_dielectrica, V_j, donadores, aceptores)
-W_bias = cal.ancho_vaciamiento_bias(cte_dielectrica, V_j, donadores)
-E_max_bias = cal.energia_maxima_bias(donadores, x_n_bias, cte_dielectrica)
+else:
+    parameter                = get_config(args.i)
+    effective_mass_holes     = float(parameter["effective_mass_holes"])
+    effective_mass_electrons = float(parameter["effective_mass_electrons"])
+    temperature              = float(parameter["temperature"])
+    gap_energy               = float(parameter["gap_energy"])
+    relative_permittivity    = float(parameter["relative_permittivity"])
+    donors                   = float(parameter["donors"])
+    acceptors                = float(parameter["acceptors"])
+    bias                     = float(parameter["bias"])
 
-'########################### RESULTADOS A UN TXT ###########################'
 
-file_name = args.n or "datos"
+n_i, N_V, N_C = cal.intrinsic_concentration(temperature, gap_energy)
+p_p, n_n, p_n, n_p = cal.carriers(n_i, donors, acceptors)
+V_bi = cal.built_in_potencial(donors, acceptors, n_i, temperature)
+x_n = cal.depletion_n(relative_permittivity, V_bi, donors, acceptors)
+x_p = cal.depletion_p(relative_permittivity, V_bi, donors, acceptors)
+W = cal.depletion_zone(x_n, x_p)
+E_max = cal.field_maximum(donors, x_n, relative_permittivity)
+ValToIn, CondToIn, p_type, n_type = cal.bands(gap_energy, temperature, acceptors, n_i, donors, N_V,
+                                              effective_mass_holes, effective_mass_electrons)
+quasi_f, quasi_f_2, fermi = cal.displacement_bands(n_type, p_type, V_bi, bias)
+
+
+if bias != 0.0:
+    V_j = cal.bias(V_bi, bias)
+    x_p_bias = cal.depletion_p_bias(relative_permittivity, V_j, donors, acceptors)
+    x_n_bias = cal.depletion_n_bias(relative_permittivity, V_j, donors, acceptors)
+    W_bias = cal.depletion_zone_bias(relative_permittivity, V_j, donors)
+    E_max_bias = cal.field_maximum_bias(donors, x_n_bias, relative_permittivity)
+
+
+'''
+
+Printing the results to a file.txt
+
+'''
+
+file_name = args.o or "results"
 check = file_name
 i = 1
 while os.path.isfile(check + ".txt"):
@@ -46,53 +96,49 @@ if i > 1:
     file_name = check
 
 f = open(file_name + ".txt", 'w')
-f.writelines("----UNION PN de SI---")
+f.writelines("\t\t\t\t\t\t+---------------+\n")
+f.writelines("\t\t\t\t\t\t|  PN JUNCTION  |\n")
+f.writelines("\t\t\t\t\t\t+---------------+\n")
 f.writelines('\n\n')
-f.writelines('n_i = %.2f [cm-3]' % n_i)
-f.writelines('\n')
-f.writelines('p = p_p = %f [cm-3]' % p_p)
-f.writelines('\n')
-f.writelines('n = n_n = %f [cm-3]' % n_n)
-f.writelines('\n')
-f.writelines('p_n = %f [cm-3]' % p_n)
-f.writelines('\n')
-f.writelines('n_p = %f [cm-3]' % n_p)
-f.writelines('\n\n\n')
-f.writelines('V_bi = %f' % V_bi)
+f.writelines('+------------Initial Configuration------------+\n')
+f.writelines("\t{:24} = {:{}{}.{}}[{}] \n".format('Temperature', temperature, '<', 10, '2f', 'K'))
+f.writelines('\t{:24} = {:{}{}.{}}[{}] \n'.format('Energy gap', gap_energy, '<', 10, '2f', 'eV'))
+f.writelines('\t{:24} = {:{}{}.{}}[{}] \n'.format('Donors', donors, '<', 10, '2e', 'cm-3'))
+f.writelines('\t{:24} = {:{}{}.{}}[{}] \n'.format('Acceptors', acceptors, '<', 10, '2e', 'cm-3'))
+f.writelines('\t{:24} = {:{}{}.{}}[{}] \n'.format('Bias', bias, '<', 10, '2f', 'V'))
+f.writelines('\t{:24} = {:{}{}.{}} \n'.format('Effective mass holes', effective_mass_holes, '<', 10, '2f'))
+f.writelines('\t{:24} = {:{}{}.{}} \n'.format('Effective mass electrons', effective_mass_electrons, '<', 10, '2f'))
+f.writelines('\t{:24} = {:{}{}.{}} \n'.format('Relative permittivity', relative_permittivity, '<', 10, '2f'))
+f.writelines('+---------------------------------------------+\n')
 f.writelines('\n\n')
-f.writelines('x_n = %f [cm]' % x_n)
-f.writelines('\n')
-f.writelines('x_p = %f [cm]' % x_p)
-f.writelines('\n')
-f.writelines('W = %f [cm]' % W)
-f.writelines('\n\n')
-f.writelines('Campo maximo = %f' % E_max)
-f.writelines('\n\n')
-f.writelines('E_i - E_V = %f' % ValToIn)
-f.writelines('\n')
-f.writelines('E_C - E_i = %f' % CondToIn)
-f.writelines('\n')
-f.writelines('Zona P: E_F - E_i = %f' % p_type)
-f.writelines('\n')
-f.writelines('Zona N: E_F - E_i = %f' % n_type)
-f.writelines('\n')
-f.writelines('\n')
-f.writelines('Deplazamiento: (E_i)_p -(E_i)_n = %f' % quasi_f)
-f.writelines('\n')
-f.writelines('Comprobacion del desplazamiento: %f' % quasi_f_2)
-f.writelines('\n\n\n\n')
-f.writelines('Polarizacion aplicada de %fV' % polarizacion)
-f.writelines('\n\n')
-f.writelines('V_j = %f' % V_j)
-f.writelines('\n\n')
-f.writelines('(x_n)\' = %f' % x_n_bias)
-f.writelines('\n')
-f.writelines('(x_p)\' = %f' % x_p_bias)
-f.writelines('\n')
-f.writelines('W\' = %f' % W_bias)
-f.writelines('\n')
-f.writelines('(E_max)\' = %f' % E_max_bias)
-f.writelines('\n\n')
-f.writelines('Desdoblamiento: %f' % fermi)
-f.writelines('\n\n')
+f.writelines('+-------------------Results-------------------+\n')
+f.writelines("\t{:7} = {:{}{}.{}}[{}] \n".format('n_i', n_i, '<', 10, '2e', 'cm-3'))
+f.writelines("\t{:7} = {:{}{}.{}}[{}] \n".format('p = p_p', p_p, '<', 10, '2e', 'cm-3'))
+f.writelines("\t{:7} = {:{}{}.{}}[{}] \n".format('n = n_n', n_n, '<', 10, '2e', 'cm-3'))
+f.writelines("\t{:7} = {:{}{}.{}}[{}] \n".format('p_n', p_n, '<', 10, '2e', 'cm-3'))
+f.writelines("\t{:7} = {:{}{}.{}}[{}] \n".format('n_p', n_p, '<', 10, '2e', 'cm-3'))
+f.writelines('                   --------                    \n')
+f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('E_i - E_V', ValToIn, '<', 10, '3f', 'eV'))
+f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('E_C - E_i', CondToIn, '<', 10, '3f', 'eV'))
+f.writelines("\t{:15} = {:{}{}.{}}[{}] (P region) \n".format('E_F - E_i', p_type, '<', 10, '3f', 'eV'))
+f.writelines("\t{:15} = {:{}{}.{}}[{}] (N region)\n".format('E_F - E_i', n_type, '<', 10, '3f', 'eV'))
+f.writelines("\t{:15} = {:{}{}.{}}[{}] (Energy displacement)\n".format('(E_i)p - (E_i)n', quasi_f, '<', 10, '3f', 'V'))
+f.writelines("\t{:15} = {:{}{}.{}}[{}] (Validation of the energy displacement)\n".format('(E_i)p - (E_i)n', quasi_f_2, '<', 10, '3f', 'V'))
+f.writelines('                   --------                    \n')
+f.writelines("\t{:5} = {:{}{}.{}}[{}] \n".format('V_bi', V_bi, '<', 10, '3f', 'V'))
+f.writelines("\t{:5} = {:{}{}.{}}[{}] \n".format('x_n', x_n, '<', 10, '2e', 'cm'))
+f.writelines("\t{:5} = {:{}{}.{}}[{}] \n".format('x_p', x_p, '<', 10, '2e', 'cm'))
+f.writelines("\t{:5} = {:{}{}.{}}[{}] \n".format('W', W, '<', 10, '2e', 'cm'))
+f.writelines("\t{:5} = {:{}{}.{}}[{}] \n".format('E_max', E_max, '<', 10, '2e', 'V/cm'))
+f.writelines('                   --------                    \n')
+f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('V_A', bias, '<', 10, '3f', 'V'))
+if bias != 0.0:
+    f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('V_j', V_j, '<', 10, '3f', 'V'))
+    f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('(x_n)\'', x_n_bias, '<', 10, '2e', 'cm'))
+    f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('(x_p)\'', x_p_bias, '<', 10, '2e', 'cm'))
+    f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('W\'', W_bias, '<', 10, '2e', 'cm'))
+    f.writelines("\t{:15} = {:{}{}.{}}[{}] \n".format('E_max', E_max_bias, '<', 10, '2e', 'V/cm'))
+    f.writelines(
+        "\t{:15} = {:{}{}.{}}[{}] (Energy displacement)\n".format('(E_i)p - (E_i)n', fermi, '<', 10, '3f', 'V'))
+f.writelines('+---------------------------------------------+\n')
 f.close()
